5ad010ee2e71d7f8cd15e6a4d9c2024b83c29d86
CodeAadarsh <aadarsh24lv@gmail.com>
2023-07-18 19:03:05 +0530

    js1

---
+++ learning_Css/JavaScript/JS1/index.js
@@ -0,0 +1,23 @@
+// alert("Hello")
+console.log("hello this is from script")
+let firstName = "Aadarsh"; 
+let num = 1234; //old
+console.log(num)
+num = 5678 // old
+console.log(num)
+num = 123456789 //latest
+const ID_NUM = 123;
+console.log('ID_NUM',ID_NUM)
+// ID_NUM = 43;
+// console.log('ID_NUM',ID_NUM)
+// PascalCase
+
+// camelCase
+
+// snake_case
+
+// kebab-case
+
+// CONSTANT_CASE
+
+console.log('2'==2) 
