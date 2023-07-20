6c9415ed57c6c8557f36e9028eae7df7881c5feb
CodeAadarsh <aadarsh24lv@gmail.com>
2023-07-20 19:09:13 +0530

    js functions and operators

--- learning_Css/JavaScript/JS1/index.js
+++ learning_Css/JavaScript/JS1/index.js
@@ -1,23 +1,120 @@
-// alert("Hello")
-console.log("hello this is from script")
-let firstName = "Aadarsh"; 
-let num = 1234; //old
-console.log(num)
-num = 5678 // old
-console.log(num)
-num = 123456789 //latest
-const ID_NUM = 123;
-console.log('ID_NUM',ID_NUM)
-// ID_NUM = 43;
+// // alert("Hello")
+// console.log("hello this is from script")
+// let firstName = "Aadarsh"; 
+// let num = 1234; //old
+// console.log(num)
+// num = 5678 // old
+// console.log(num)
+// num = 123456789 //latest
+// const ID_NUM = 123;
 // console.log('ID_NUM',ID_NUM)
-// PascalCase
+// // ID_NUM = 43;
+// // console.log('ID_NUM',ID_NUM)
+// // PascalCase
 
-// camelCase
+// // camelCase
 
-// snake_case
+// // snake_case
 
-// kebab-case
+// // kebab-case
 
-// CONSTANT_CASE
+// // CONSTANT_CASE
+// // console.log('2'===2) 
 
-console.log('2'==2) 
+// var nameX = "Mango" //ES5
+// let nameX1 = "Adarsh" //ES6
+// const nameX2 = "Adarsh" //ES6
+// key: value
+// nameX: "Mango"
+
+// let n;
+// n  = 2;
+// console.log(n)
+
+// {
+//     let q= 12
+//     console.log(q) // 12
+//    {
+//     let z = 23
+//      q=q+q;
+//      console.log(q)//24
+//      {
+//         let a= 25
+//         console.log(a)
+//         {
+//             var x = 123
+//         }
+//         console.log(a)
+//     }
+//     console.log(q)
+//     }
+//     console.log(q)//24
+// }
+// console.log(n)
+
+// Function in Javascript
+// var a=12;
+var b=13;
+// function func1(a,b){
+//     console.log((a*b)*(100/8+5));
+//     console.log("the value of a is:",a );
+//     console.log("the value of b is:",b );
+
+// }
+
+// func1(12,14);
+// func1(1,4);
+// func1(100,8);
+
+function greet(){
+    console.log("Welcome to javascript!")
+}
+greet()
+
+function greetWithName(name){
+    console.log("Welcome",name,"sir!");
+}
+var xyz = "Suraj"
+greetWithName(xyz)
+
+
+var x = 1;
+function changeValue(a){
+    a= a+5;
+    return a;
+} 
+
+let funcVar = changeValue(x);
+
+// operators in javascript
+
+// + add
+// - subs
+// * multiply
+// ** expo
+//  % mod
+// / divide
+// ++ increment
+// -- decrement
+
+// arrow functions
+
+let i = () =>{
+    return "hello javascript"
+} 
+
+let af = 2
+
+console.log(12+12, "+")
+console.log(12*12, "*")
+console.log(12/9, "/")
+console.log(12%9, "%")
+console.log(2**3, "**")
+console.log(++af, "++")
+console.log(af--, "--")
+
+// number --  7 7.8 
+// String "asasas" 'saas' `asas` 
+// Boolean true false
+// undefined 
+// null 
