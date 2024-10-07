ffcf56f99897aa05af6a7a56e377d031dc6b2795
Shiv Kumar <140259236+shiv90154@users.noreply.github.com>
2024-10-08 04:36:14 +0530

    Add files via upload

---
+++ New folder/script.js
@@ -0,0 +1,51 @@
+// var a = 20;
+// var b = 12;
+// var c = a + b;
+// document.write(c);
+// // console.log(c)
+
+// const { log } = require("console");
+
+
+
+// global variable
+// var a = 2000;
+// function first() {
+//     console.log(a);
+// }
+// function second() {
+//     console.log(a)
+
+// }
+// first();
+// second();
+
+// console.log(a);
+// // local variable /
+
+// function L() {
+//     var l = 23;
+//     console.log(l);
+
+// }
+// L();
+// console.log(l); error
+
+
+// Array in java script
+// var arr = ["shiv", "shiv2", "shiv 3"];
+// for (i = 0; i < arr.length; i++) {
+//     document.write(arr[i] + "<br/>");
+// }
+// console.log(arr);
+
+
+
+// var i;
+// var emp = new Array();
+// emp[0] = "Arun";
+// emp[1] = "Varun";
+// emp[2] = "John";
+// for (i = 0; i < emp.length; i++) {
+//     document.write(emp[i] + "<br>");
+// }
